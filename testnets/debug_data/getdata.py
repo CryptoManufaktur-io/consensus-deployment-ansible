568ed5d765021abeda7e5bfdfc6c26afd76955be
George Gatheca <gatheca.george@gmail.com>
2022-10-27 16:46:18 +0100

    get forkchoice dump

--- testnets/debug_data/getdata.py
+++ testnets/debug_data/getdata.py
@@ -1,26 +1,34 @@
-from re import S
-import requests
-import pathlib
-import calendar
-import time
-import os
+import requests, sys, pathlib, calendar, time, os
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 
 gmt = time.gmtime()
 ts = calendar.timegm(gmt)
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
-dir_name = dir_path + "/" + str(ts) + "_after_rejoin"
-pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
 
-data_loader = DataLoader()
-inventory = InventoryManager(loader = data_loader, sources=[inventory_file_name])
-hostsList = inventory.get_hosts()
-print(hostsList)
+if __name__ == '__main__':
+  append = sys.argv[1] if len(sys.argv) == 2 else ''
+  dir_name = dir_path + "/data/" + str(ts) + append
+  pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
 
-# for server in servers:
-#   url = "http://" + servers[server] + ":4000/eth/v1/debug/beacon/forkchoice"
-#   solditems = requests.get(url)
-#   pathlib.Path(dir_name + '/data_' + server + '.json').write_bytes(solditems.content)
+  inventory_file_name = dir_path + "/../efoundation/inventory/inventory.ini"
+  data_loader = DataLoader()
+  inventory = InventoryManager(loader = data_loader, sources=[inventory_file_name])
+  hostsList = inventory.get_hosts()
+
+  for host in hostsList:
+    label = str(host)
+    ip = host.vars['ansible_host']
+
+    if(label.__contains__('prysm')):
+      url = "http://" + ip + ":4000/eth/v1/debug/beacon/forkchoice"
+    elif(label.__contains__('teku')):
+      url = "http://" + ip + ":4000/teku/v1/debug/beacon/protoarray"
+    elif(label.__contains__('lighthouse')):
+      url = "http://" + ip + ":4000/lighthouse/proto_array"
+    else:
+      continue
+
+    solditems = requests.get(url)
+    pathlib.Path(dir_name + '/' + label + '.json').write_bytes(solditems.content)
 
