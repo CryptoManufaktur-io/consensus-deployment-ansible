12f71660dff74ece77de75d867bd2f0a03344453
George Gatheca <gatheca.george@gmail.com>
2023-02-28 21:04:10 +0000

    Fix python script to get tx details

--- testnets/efoundation/premerge_custom_conf_data/transaction.py
+++ testnets/efoundation/premerge_custom_conf_data/transaction.py
@@ -12,11 +12,12 @@ def getSoup(url):
 def getDetails(base_url, transaction_hash):
   soup = getSoup(base_url + "/tx/" + transaction_hash)
   transaction_hash = soup.find(id="spanTxHash").text
-  contract_address = soup.find(id="contractCopy").text
+  #contract_address = soup.find(id="contractCopy").text
+  contract_address = soup.find_all("a", href=lambda href: href and "/address/" in href)[1].text
   block_number = soup.find_all("a", href=lambda href: href and "/block/" in href)[0].text
 
   soup = getSoup(base_url + "/block/" + block_number)
-  block_hash = soup.find(id="collapsePanel").find_all("div")[0].find_all("div")[1].text.replace("\n", "")
+  block_hash = soup.find(id="collapseContent").find_all("div")[0].find_all("div")[1].text.replace("\n", "")
 
   return {
     "transaction_hash": transaction_hash, 
